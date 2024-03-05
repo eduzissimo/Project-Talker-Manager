0d581ba46bb26c0034f3e7c5668df566f570ba74
eduzissimo <eduardo.alves14@hotmail.com>
2024-03-04 23:16:58 -0300

    Feat: Implementando as formas de validação do login

--- src/routes/LoginRoutes.js
+++ src/routes/LoginRoutes.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 const LoginData = require('../utils/addLogin');
+const validateLogin = require('../middlewares/validateLogins');
 
-router.post('/', LoginData.tokenLogin);
+router.post('/', validateLogin, LoginData.tokenLogin);
 
 module.exports = router;
